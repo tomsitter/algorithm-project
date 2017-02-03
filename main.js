90973e6456fa185b60b2484d45f3919bddf40062
Tom Sitter <thomas.sitter@gmail.com>
2017-02-02 23:22:28 -0500

    Minor updates

--- main.js
+++ main.js
@@ -6,5 +6,4 @@ const {app, BrowserWindow} = electron
 app.on('ready', () => {
     let win = new BrowserWindow({width: 600, heigh: 600})
     win.loadURL(`file://${__dirname}/index.html`)
-    win.webContents.openDevTools()
 })
