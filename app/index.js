f4512424fb8a104d0cdf33a97654f6a88afb186a
Tom Sitter <thomas.sitter@gmail.com>
2017-02-08 16:23:52 -0500

    Abstract charting to seperate file

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const {dialog} = require('electron').remote;
 
 const d3 = require("d3");
 const parsers = require("./js/parsers");
+const charts = require("./js/charts");
    
 const fileManagerBtn = document.getElementById('open-file-manager')
 
@@ -18,59 +19,18 @@ fileManagerBtn.addEventListener('click', () => {
 })
 
 function plot(err, raw) {
-    var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var parseTime = d3.timeParse("%d-%b-%y");
-
-    var x = d3.scaleTime()
-        .rangeRound([0, width]);
-
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-    var line = d3.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.close); });
-
     let data = d3.csvParse(raw, normalizeData);
+    let dmMonths = data.map(function(a) {return a["DM Months"];});
     
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.close; }));
-
-    g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-    .select(".domain")
-      .remove();
-
-    g.append("g")
-      .call(d3.axisLeft(y))
-    .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Price ($)");
-
-    g.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
+    let exampleChart = charts.barChart();
+    d3.select("#d3-chart").datum(dmMonths).call(exampleChart);
 }
 
 function normalizeData(row) {
   if (state.EMR=="PSS") {
-    row["Date Hb A1C"] = new Date(row["Date Hb A1C"]);
-    row["Date LDL"] = parsers.parseDate(state.EMR, row["Date LDL"]);
+    row["Date Hb A1C"] = new Date(row["Date Hb A1C"])
+    row["Date LDL"] = parsers.parseDate(state.EMR, row["Date LDL"])
+    row["DM Months"] = +row["DM Months"]
   }
   return row
 }
---
+++ app/js/charts.js
@@ -0,0 +1,62 @@
+// Reusable d3 charts
+
+const d3 = require('d3')
+
+function barChart() {
+    let width = 360,
+        height = 360,
+        barPadding = 1,
+        fillColor = 'steelblue';
+        
+    function my(selection) {
+        selection.each(function(data) {
+            var barSpacing = height / data.length,
+                barHeight = barSpacing - barPadding,
+                maxValue = d3.max(data),
+                widthScale = width / maxValue;
+            
+            d3.select(this).append('svg')
+                .attr('height', height)
+                .attr('width', width)
+                .selectAll('rect')
+                .data(data)
+                    .enter()
+                .append('rect')
+                .attr('y', function(d, i) { return i * barSpacing})
+                .attr('height', barHeight)
+                .attr('x', 0)
+                .attr('width', function(d) { return d * widthScale})
+                .style('fill', fillColor);
+        });
+    }
+    
+    my.width = function(value) {
+        if (!arguments.length) return width;
+        width = value
+        return my
+    };
+    
+    my.height = function(value) {
+        if (!arguments.length) return height;
+        height = value
+        return my
+    }
+    
+    my.barPadding = function(value) {
+        if (!arguments.length) return barPadding;
+        barPadding = value
+        return my
+    };
+ 
+    my.fillColor = function(value) {
+        if (!arguments.length) return fillColor;
+        fillColor = value
+        return my
+    };
+    
+    return my
+}
+
+module.exports = {
+    'barChart': barChart
+}
